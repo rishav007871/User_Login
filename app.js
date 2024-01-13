d1b8700f31ea207f50636479a3ecb269157edf19
rishav007871 <79625398+rishav007871@users.noreply.github.com>
2024-01-13 13:53:42 +0530

    Add Cookies and Sessions

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express()
 
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(session({
+    secret: 'This is our Secret.',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB")
 
@@ -19,7 +29,14 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home")
@@ -33,53 +50,53 @@ app.get("/register", function(req, res) {
     res.render("register")
 })
 
-app.post("/register", function(req, res) {
-    bcrypt.hash(req.body.password, saltRounds)
-        .then(function(hash) {
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
+app.get("/secrets", function(req, res) {
+    if(req.isAuthenticated()) {
+        res.render("secrets")
+    } else {
+        res.render("login")
+    }
+})
 
-            newUser.save()
-                .then(() => {
-                    res.render("secrets");
-                })
-                .catch((err) => {
-                    console.log("Error Occurred:", err);
-                });
-        })
-        .catch((err) => {
-            console.log("Error Occurred while hashing password:", err);
-        });
+app.get("/logout", function(req, res){
+    req.logout(function(err) {
+        if (err) {
+            console.log("Error during logout:", err);
+        }
+        res.redirect("/");
+    });
+});
+
+app.post("/register", function(req, res) {
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err) {
+            console.log("Some Error Occurred")
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                console.log("Registration Successful")
+                res.redirect("/secrets");
+              });
+        }
+    })
 });
 
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
 
-    User.findOne({ email: username })
-        .then((foundUser) => {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password)
-                    .then(function(result) {
-                        if (result) {
-                            res.render("secrets");
-                            console.log("Login Successful");
-                        } else {
-                            console.log("Incorrect password");
-                        }
-                    })
-                    .catch((err) => {
-                        console.log("Error Occurred while comparing passwords:", err);
-                    });
-            } else {
-                console.log("User not found");
-            }
-        })
-        .catch((err) => {
-            console.log("Some Error Occurred:", err);
-        });
+    req.login(user, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          passport.authenticate("local")(req, res, function() {
+            console.log("Login Successful")
+            res.redirect("/secrets");
+          });
+        }
+      });    
 });
 
 app.listen(3000, function() {
